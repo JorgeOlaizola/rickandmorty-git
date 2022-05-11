e148bd3e92a7b3587b7c1e404d922d7b45c9268e
Jorge Olaizola <jorgelolaizola@hotmail.com>
2022-05-11 15:36:42 -0300

    Deleted fetch

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React from 'react'
 import { API_ENDPOINTS } from './utils/constants';
 
 function App() {
-  React.useEffect(() => {
-    fetch(API_ENDPOINTS.episodes).then(r => r.json()).then(r => console.log(r))
-  }, [])
   return (
     <div>
       Rick and morty
