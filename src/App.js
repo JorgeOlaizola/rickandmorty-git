f393a7c495102bf2230d5c27b93dd89e1d366161
Jorge Olaizola <jorgelolaizola@hotmail.com>
2022-05-11 15:34:59 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { API_ENDPOINTS } from './utils/constants';
 
 function App() {
+  React.useEffect(() => {
+    fetch(API_ENDPOINTS.episodes).then(r => r.json()).then(r => console.log(r))
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Rick and morty
     </div>
   );
 }
---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+const FETCH_URL = 'https://rickandmortyapi.com/api'
+
+export const API_ENDPOINTS = {
+    characters: `${FETCH_URL}/character`,
+    locations: `${FETCH_URL}/location`,
+    episodes: `${FETCH_URL}/episode`
+}
