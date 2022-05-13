4bf0377252849d9cf90b09e2bea09bd98aa6c43d
deborah1993 <deborah.sanchez1993@gmail.com>
2022-05-13 20:46:20 -0300

    Home

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from 'react'
-import { Routes, Route} from 'react-router-dom'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home.js";
 
 function App() {
   return (
     <div>
       Rick and morty
       <Routes>
-        <Route path='/' element={<div>Home</div>}/>
-        <Route path='/characters' element={<div>Characters</div>}/>
-        <Route path='/episodes' element={<div>Episodes</div>}/>
-        <Route path='/locations' element={<div>Locations</div>}/>
-        <Route path='/contact' element={<div>Contact</div>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/characters" element={<div>Characters</div>} />
+        <Route path="/episodes" element={<div>Episodes</div>} />
+        <Route path="/locations" element={<div>Locations</div>} />
+        <Route path="/contact" element={<div>Contact</div>} />
       </Routes>
     </div>
   );
