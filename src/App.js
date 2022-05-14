85d84e6ff93405b9a87c833b1e5d7a2da2c93ebe
Jorge Olaizola <jorgelolaizola@hotmail.com>
2022-05-13 21:36:01 -0300

    Imported styles in app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Routes, Route} from 'react-router-dom'
 import { Navbar } from './components/NavBar/Navbar';
+import './styles/global.css'
 
 function App() {
   return (
