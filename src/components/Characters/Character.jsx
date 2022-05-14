4ceea45d2cc4190bc5fe83311d7af1e619e61b0f
Jorge Olaizola <jorgelolaizola@hotmail.com>
2022-05-13 21:54:22 -0300

    update

--- src/components/Characters/Character.jsx
+++ src/components/Characters/Character.jsx
@@ -1,9 +1,11 @@
+import './characters.css'
+
 export default function Character({ character }) {
   return (
-    <div>
-      <span>{character.name}</span>
-      <img src={character.image} alt="Character img" />
-      <span>{character.species}</span>
+    <div className='character-container'>
+      <span  className='character-name' >{character.name}</span>
+      <img className='character-image' src={character.image} alt="Character img" />
+      <span className='character-species' >{character.species}</span>
     </div>
   );
 }
--- src/components/Characters/Characters.jsx
+++ src/components/Characters/Characters.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { API_ENDPOINTS } from "../../utils/constants";
 import Character from "./Character";
+import './characters.css'
 
 export default function CharactersComponent() {
   const [characters, setCharacters] = useState();
@@ -13,7 +14,7 @@ export default function CharactersComponent() {
   }, []);
 
   return characters ? (
-    <div>
+    <div className='characters-container'>
       {characters?.map((character) => (
         <Character key={character.id} character={character} />
       ))}
